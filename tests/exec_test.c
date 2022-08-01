d274c7ef27ef0bc09d5e9b7b098c229eb57f55d8
____marcell <0000marcell@gmail.com>
2022-08-01 11:41:17 +0000

    Solved AST for second example in tests

--- tests/exec_test.c
+++ tests/exec_test.c
@@ -32,83 +32,139 @@ void first_case() {
             },
             .return_value = 99
           },
-          .return_value = 99
         },
         .return_value = 99
       }
-    }
+    },
+    .return_value = 99
   }; 
-  global.type = "global"; 
-  struct scope fibfunc;
-  fibfunc.type = "fib";
-  arg arg1 = {
-    .key = "n",
-    .value = 10
-  };
-  fibfunc.args[0] = arg1;
-  global.scopes[0] = &fibfunc;
-  global.scopescount = 1;
-  struct scope ifst;
-  ifst.type = "if";
-  ifst.value = 1;
-  struct scope nst; 
-  nst.type = "number";
-  nst.value = 99;
-  ifst.scopes[0] = &nst;
-  ifst.scopescount = 1;
-  fibfunc.scopes[0] = &ifst;
-  fibfunc.scopescount = 1;
   exec(&global);
   printf(">>>>> return_value %d\n", global.scopes[0]->return_value);
-  assert(global.scopes[0]->return_value == 99);
+  assert(global.scopes[0]->return_value == 0);
 }
 
 void second_case() {
-  // function with one if statement returning 99
+  // function with two if statement returning 99
   printf("running tests...\n");
-  struct scope global; 
-  global.type = "global"; 
-  struct scope fibfunc;
-  fibfunc.type = "fib";
-  arg arg1 = {
-    .key = "n",
-    .value = 10
+  struct scope global = {
+    .type = "global",
+    .scopescount = 1,
+    .scopes = {
+      &(struct scope) {
+        .type = "fib",
+        .args = {
+          {
+            .key = "n",
+            .value = 1 
+          }
+        },
+        .scopescount = 2,
+        .scopes = {
+          &(struct scope) {
+            .type = "if",
+            .value = 0,
+            .scopescount = 1,
+            .scopes = {
+              &(struct scope) {
+                .type = "number",
+                .value = 0
+              }
+            },
+            .return_value = 99
+          },
+          &(struct scope) {
+            .type = "if",
+            .value = 1,
+            .scopescount = 1,
+            .scopes = {
+              &(struct scope) {
+                .type = "number",
+                .value = 1
+              }
+            },
+            .return_value = 99
+          },
+        },
+        .return_value = 99
+      }
+    },
+    .return_value = 99
   };
-  fibfunc.args[0] = arg1;
-  global.scopes[0] = &fibfunc;
-  global.scopescount = 1;
-  struct scope ifst;
-  ifst.type = "if";
-  ifst.value = 0;
-  struct scope nst; 
-  nst.type = "number";
-  nst.value = 88;
-  ifst.scopes[0] = &nst;
-  ifst.scopescount = 1;
-  fibfunc.scopes[0] = &ifst;
-  fibfunc.scopescount = 1;
-
-  struct scope ifst2;
-  ifst2.type = "if";
-  ifst2.value = 1;
-  struct scope nst2; 
-  nst2.type = "number";
-  nst2.value = 99;
-  ifst2.scopes[0] = &nst2;
-  ifst2.scopescount = 1;
-  fibfunc.scopes[1] = &ifst2;
-  fibfunc.scopescount = 2;
   exec(&global);
   printf(">>>>> return_value %d\n", global.scopes[0]->return_value);
-  assert(global.scopes[0]->return_value == 99);
+  assert(global.scopes[0]->return_value == 1);
+}
+
+void third_case() {
+  // function with recursive calls, fib > 2
+  printf("running tests...\n");
+  struct scope global = {
+    .type = "global",
+    .scopescount = 1,
+    .scopes = {
+      &(struct scope) {
+        .type = "fib",
+        .args = {
+          {
+            .key = "n",
+            .value = 1 
+          }
+        },
+        .scopescount = 2,
+        .scopes = {
+          &(struct scope) {
+            .type = "if",
+            .value = 0,
+            .scopescount = 1,
+            .scopes = {
+              &(struct scope) {
+                .type = "number",
+                .value = 0
+              }
+            },
+            .return_value = 99
+          },
+          &(struct scope) {
+            .type = "if",
+            .value = 0,
+            .scopescount = 1,
+            .scopes = {
+              &(struct scope) {
+                .type = "number",
+                .value = 1
+              }
+            },
+            .return_value = 99
+          },
+          &(struct scope) {
+            .type = "if",
+            .value = 1,
+            .scopescount = 1,
+            .scopes = {
+              &(struct scope) {
+                .type = "number",
+                .value = 1
+              }
+            },
+            .return_value = 99
+          },
+        },
+        .return_value = 99
+      }
+    },
+    .return_value = 99
+  };
+  exec(&global);
+  printf(">>>>> return_value %d\n", global.scopes[0]->return_value);
+  assert(global.scopes[0]->return_value == 1);
 }
 
 int main() {
   // function with one if statement returning 99
   //first_case();
-
   // function with two if statements second one returning 99 first returning 0
-  second_case();
+  //second_case();
 
+  third_case();
   return 0;
 }
